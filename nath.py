5264ce0fafd45308040197d735658f519cd220f0
NathQuintero <141141023+NathQuintero@users.noreply.github.com>
2025-03-28 21:52:55 -0500

    Update nath.py

--- nath.py
+++ nath.py
@@ -138,28 +138,6 @@ st.write("Desarrollo del Proyecto de Ciencia de Datos con Redes Convolucionales"
 st.subheader("Identificación de objetos con VGG16")
 confianza = st.slider("Seleccione el nivel de confianza", 0, 100, 50) / 100
 
-option = st.selectbox(
-    "¿Qué te gustaría usar para subir la foto?",
-    ("Tomar foto", "Subir archivo", "URL"),
-    index=None,
-    placeholder="Selecciona cómo subir la foto"
-)
-confianza = st.slider("Seleccione el nivel de confianza", 0, 100, 50) / 100
-
-img_file_buffer = None
-
-if option == "Tomar foto":
-    img_file_buffer = st.camera_input("Capture una foto para identificar el producto")
-elif option == "Subir archivo":
-    img_file_buffer = st.file_uploader("Cargar imagen desde archivo", type=["jpg", "jpeg", "png"])
-elif option == "URL":
-    image_url = st.text_input("O ingrese la URL de la imagen")
-    if image_url:
-        try:
-            response = requests.get(image_url)
-            img_file_buffer = BytesIO(response.content)
-        except Exception as e:
-            st.error(f"Error al cargar la imagen desde la URL: {e}")
             
 def preprocess_image(image):
     if image.mode != 'RGB':
